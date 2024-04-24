5ac75d680db5773571d9addef1f1d98f987477d4
Shreyash-69 <bhosaleshreyash2@gmail.com>
2024-04-24 21:01:52 +0530

    implemented db setup with prisma

--- api/app.js
+++ api/app.js
@@ -1,11 +1,16 @@
 import express from 'express'
 
+import postRoute from './routes/posts.js'
+import authRoute from './routes/auth.js'
 
 
 
 const app=express()
 
+app.use(express.json())
 
+app.use('/api/auth',authRoute)
+app.use('api/posts',postRoute)
 console.log('test')
 
 app.listen(3000,()=>{
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,15 @@
-export const register=(req,res)=>{
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import User from '../models/User.js'
+
+
+export const register=async(req,res)=>{
+    const {username,email,password}=req.body
     try{
+
+        //password hashing
+
+        const hashedPass=await bcrypt.hash(password,10)
 
     }catch(error){
 
