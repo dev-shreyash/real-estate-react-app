0afd9fc142b1e2466221a27f979e8e33d2839b61
Shreyash-69 <bhosaleshreyash2@gmail.com>
2024-04-26 16:29:07 +0530

    implementing auth controllers

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,33 @@
 import bcrypt from 'bcrypt'
 import prisma from '../lib/prisma.js'
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from '../utils/asyncHandler.js';
+  
 
-export const register=async(req,res)=>{
+
+export const register=asyncHandler(async(req,res)=>{
     const {username,email,password}=req.body
+
+    if (!(username&&email&&password)) {
+        throw new ApiError(400,"Please provide your All Details")
+    }
+   
     try{
-
+        const existedUser = await prisma.user.findFirst({
+            where: {
+                OR: [
+                    { email: email },
+                    { username: username }
+                ]
+            }
+        });
+    
+        if (existedUser) {
+            throw new ApiError(409, 'User already exists');
+        }
         //password hashing
-
+        
         const hashedPass=await bcrypt.hash(password,10)
 
         console.log(hashedPass)
@@ -29,22 +50,44 @@ export const register=async(req,res)=>{
             message:"Something went wrong while creating new user",
         })
     }
-}
+})
 
-export const login=(req,res)=>{
+export const login=asyncHandler(async(req,res)=>{
     const {username, password}=req.body
-
-    if (!username) {
-        res.json
+    console.log(username,password)
+    if (!(username&&password)) {
+        
+        throw new ApiError(400,"plz provide username and password")
+        
     }
     try{
+        const user = await prisma.user.findFirst({
+            where: {
+                OR: [
+                    { email: email },
+                    { username: username }
+                ]
+            }
+        })
+        if(!user){
+            throw new ApiError(400,"user dose not exist")
+        }
+    
+        //check pass
 
+        const isPasswordValid=await bcrypt.compare(password,user.password)
+        if(!isPasswordValid){
+            throw new ApiError(400,"password is not valid")
+        }
+        
+        //cookie tokes
 
+       res.setHeader("set-Cookie","test="+"myValue").json("success")
     }catch(error){
 
     }
 
-}
+})
 export const logout=(req,res)=>{
     try{
 
