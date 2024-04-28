8bde2997b9ff04c18213b6a957dd376056b41c59
Shreyash-69 <bhosaleshreyash2@gmail.com>
2024-04-28 20:32:17 +0530

    Update Slider component in SinglePage and ProfilePage; add Map component to SinglePage

--- client/src/routes/singlePage/SinglePage.jsx
+++ client/src/routes/singlePage/SinglePage.jsx
@@ -1,18 +1,20 @@
 import Slider from '../../components/slider/Slider'
 import './singlePage.scss'
 import { singlePostData, userData } from '../../lib/dummyData'
+import Map from '../../components/map/Map';
+
 function SinglePage() {
     return (
         <div className='singlePage'>
             <div className="details">
                 <div className="wrapper">
-                    <Slider images={singlePostData.images}/>
+                    <Slider images={singlePostData.images} />
                     <div className="info">
                         <div className="top">
                             <div className="post">
                                 <h1>{singlePostData.title}</h1>
                                 <div className="address">
-                                    <img src="/pin.png" alt=""/>
+                                    <img src="/pin.png" alt="" />
                                     <span>{singlePostData.address}</span>
                                 </div>
                                 <div className="price">
@@ -24,12 +26,94 @@ function SinglePage() {
                                 <span>{userData.name}</span>
                             </div>
                         </div>
-                        <div className="bottom"></div>
+                        <div className="bottom">
+                            {singlePostData.description}
+                        </div>
                     </div>
                 </div>
             </div>
             <div className="features">
-                <div className="wrapper"></div>
+                <div className="wrapper">
+                    <p className="title">General</p>
+                    <div className="listVertical">
+                        <div className="feature">
+                            <img src="/utility.png" alt="" />
+                            <div className="featureText">
+                                <span>Utilities</span>
+                                <p>Renter is responsible</p>
+                            </div>
+                        </div>
+                        <div className="feature">
+                            <img src="/pet.png" alt="" />
+                            <div className="featureText">
+                                <span>Pet Policy</span>
+                                <p>Pets Allowed</p>
+                            </div>
+                        </div>
+                        <div className="feature">
+                            <img src="/fee.png" alt="" />
+                            <div className="featureText">
+                                <span>Propeties fees</span>
+                                <p>Must have 4x the rent in total household income</p>
+                            </div>
+                        </div>
+                    </div>
+                    <p className="title">Sizes</p>
+                    <div className="sizes">
+                        <div className="size">
+                            <img src="/size.png" alt="" />
+                            <span>80 sqft</span>
+                        </div>
+                        <div className="size">
+                            <img src="/bed.png" alt="" />
+                            <span>2 Beds</span>
+                        </div>
+                        <div className="size">
+                            <img src="/bath.png" alt="" />
+                            <span>1 Bathroom</span>
+                        </div>
+                    </div>
+                    <p className="title">Nearby Places</p>
+                    <div className="listHorizontal">
+                    <div className="feature">
+                            <img src="/school.png" alt="" />
+                            <div className="featureText">
+                                <span>School</span>
+                                <p>Renter is responsible</p>
+                            </div>
+                        </div>
+                        <div className="feature">
+                            <img src="/pet.png" alt="" />
+                            <div className="featureText">
+                                <span>Bus Stops</span>
+                                <p>100m away</p>
+                            </div>
+                        </div>
+                        <div className="feature">
+                            <img src="/fee.png" alt="" />
+                            <div className="featureText">
+                                <span>Restaurants</span>
+                                <p>200m away</p>
+                            </div>
+                        </div>
+                    </div>
+                    <p className="title">Location</p>
+                        <div className="mapContainer">
+                            <Map items={[singlePostData]}/>
+                        </div>
+                        <div className="buttons">
+                            <button>
+                                <img src="/chat.png" alt="" />
+                                send a Message
+                            </button>
+                            <button>
+                                <img src="/save.png" alt="" />
+                                 save place
+                            </button>
+                        </div>
+
+
+                </div>
             </div>
         </div>
     )
